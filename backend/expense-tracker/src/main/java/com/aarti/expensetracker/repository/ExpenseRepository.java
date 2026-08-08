a4a6afdef4de094bd23ff15a8beebdba840e971d
Aarti Gore <aartigore2005@gmail.com>
2026-08-08 07:57:08 +0530

    Add dashboard analytics for listing expenses of each user

--- backend/expense-tracker/src/main/java/com/aarti/expensetracker/repository/ExpenseRepository.java
+++ backend/expense-tracker/src/main/java/com/aarti/expensetracker/repository/ExpenseRepository.java
@@ -1,5 +1,6 @@
 package com.aarti.expensetracker.repository;
 
+import com.aarti.expensetracker.dto.CategorySummaryDTO;
 import com.aarti.expensetracker.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,14 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     @Query("SELECT COALESCE(SUM(e.amount),0) FROM Expense e WHERE e.user.email=:email")
     Double getTotalExpensesByUser(@Param("email") String email);
 
+    @Query("""
+SELECT new com.aarti.expensetracker.dto.CategorySummaryDTO(
+    e.category,
+    SUM(e.amount)
+)
+FROM Expense e
+WHERE e.user.email = :email
+GROUP BY e.category
+""")
+    List<CategorySummaryDTO> getCategorySummary(@Param("email") String email);
 }
