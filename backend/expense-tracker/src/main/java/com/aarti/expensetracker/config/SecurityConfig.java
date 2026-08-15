71041b7c8713127a33c5c3f0b52364ad45ab8959
Aarti Gore <aartigore2005@gmail.com>
2026-08-15 15:24:59 +0530

    Fix CORS origin  for production frontend

--- backend/expense-tracker/src/main/java/com/aarti/expensetracker/config/SecurityConfig.java
+++ backend/expense-tracker/src/main/java/com/aarti/expensetracker/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173","http://remarkable-peace-production.up.railway.app")
+                List.of("http://localhost:5173","https://remarkable-peace-production.up.railway.app")
         );
 
         configuration.setAllowedMethods(
